fe9b38aa3be7fc395ab8bb9f5666cccef1c18090
Aleksey Ivasyuta <avivasyuta@avito.ru>
2020-10-27 21:53:25 +0300

    add solution for 03-streams 01-limit-size-stream/02-line-split-stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.sent = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    const isLimitExceeded = this.sent + chunk.length > this.limit;
 
+    if (isLimitExceeded) {
+      callback(new LimitExceededError());
+    }
+
+    this.sent += chunk.length;
+    callback(null, chunk);
   }
 }
 
